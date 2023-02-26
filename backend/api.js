9efe2bf64ca3425e788a8d3be0a067e4291cf944
Daniel Assunção <daniel_cva@msn.com>
2023-02-26 00:22:15 +0000

    Implementing routes

--- backend/api.js
+++ backend/api.js
@@ -1,11 +1,128 @@
-let app = require('express')(); // express object
-let mongoClient = require('mongodb').MongoClient; // mongodb client
-let cors = require('cors'); // cors
-let parser = require('body-parser'); // parser
-let PORT = 8080; // port for express
-let DB_URL = 'mongodb://localhost:27017'; // db url
+const app = require('express')(); // express object
+const mongoClient = require('mongodb').MongoClient; // mongodb client
+const cors = require('cors'); // cors
+const parser = require('body-parser'); // parser
+const port = 3001; // port for express
+const db_url = 'mongodb://127.0.0.1:27017'; // db url
 app.use(parser.json()); // add cors & body paraser to the express
 app.use(cors()); // enabling cors
-app.listen(PORT, () => console.log(`Server started at ${PORT}`));
+app.listen(port, () => console.log(`Server started at ${port}`));
 
-app.get('/employees', (request, response) => {});
+// function autoInc(collection) {
+// 	mongoClient
+// 		.connect(db_url, { useNewUrlParser: true })
+// 		.then((client) => {
+//             const db = client.db('mydb');
+// 			const doc = db.collection('auto_increment').findOne({_id: collection});
+//             doc.then((document) => {
+//                 const id = document.id;
+//                 db.collection('auto_increment').updateOne({_id: collection}, {$set: {id:id+1}})
+//             })
+//         })
+// }
+
+app.post('/login', (request, response) => {
+	//user must enter auto-generated id & the password to login
+	//Check user in database with email and password
+	//If exists store session in a variable ? and return a token
+	//?store token as well to the user
+	//If not return 401, User not found, verify email and password and try again.
+});
+
+//middleware to check always the user token and email
+//If user not valid redirect to login page
+
+app.get('/logout', (request, response) => {
+	//Check user session list
+	//If exists remove session in a variable ? and return a token
+	//?store token as well to the user
+	//Redirect
+});
+
+//Get profile data
+//Logged in user can see his/her details like name, dob, phone
+app.get('/profile/:id', (request, response) => {
+	mongoClient
+		.connect(db_url, { useNewUrlParser: true })
+		.then((client) => {
+			const db = client.db('mydb');
+			const doc = db.collection('contact').findOne({ _id: request.params.id });
+			doc.then((document) => response.status(200).json(document))
+				.catch(() => response.status(404).json({ error: 'User not found' }))
+				.finally(() => client.close());
+		})
+		.catch(() => response.status(404).json({ error: 'DB_ERR' }));
+});
+
+//Create profile
+//User must enter name, password, phone, dob and contact list must be empty default
+//_id must be auto-generated
+app.post('/profile', (request, response) => {
+	mongoClient
+		.connect(db_url, { useNewUrlParser: true })
+		.then((client) => {
+			const db = client.db('mydb');
+			const doc = db
+				.collection('contact')
+				.insertOne({ _id: '1', ...request.body, contacts: [] });
+			doc.then((document) => response.status(201).json(document))
+				.catch(() => response.status(404).json({ error: 'Failed to create user profile' }))
+				.finally(() => client.close());
+		})
+		.catch(() => response.status(404).json({ error: 'DB_ERR' }));
+});
+
+//FIXME - NOT WORKING, ALWAYS GOES CATCH BLOCK
+//Update profile
+//Logged in user must able to update any of the details like name, phone, password
+app.put('/profile/:id', (request, response) => {
+	mongoClient
+		.connect(db_url, { useNewUrlParser: true })
+		.then((client) => {
+			const db = client.db('mydb');
+			const doc = db
+				.collection('contact')
+				.updateOne({ _id: request.params.id }, ...request.body);
+			doc.then((document) => response.status(200).json(document))
+				.catch(() => response.status(404).json({ error: 'Failed to update user profile' }))
+				.finally(() => client.close());
+		})
+		.catch((error) => response.status(404).json({ error: 'DB_ERR' }));
+});
+
+//Add contact
+//Logged in user must able to add contact name & phone to their profile
+app.post('/contact/:id', (request, response) => {
+	mongoClient
+		.connect(db_url, { useNewUrlParser: true })
+		.then((client) => {
+			const db = client.db('mydb');
+			const doc = db
+				.collection('contact')
+				.updateOne({ _id: request.params.id }, { $push: { contacts: request.body } });
+			doc.then((document) => response.status(201).json(document))
+				.catch(() => response.status(404).json({ error: 'Failed to add contact' }))
+				.finally(() => client.close());
+		})
+		.catch(() => response.status(404).json({ error: 'DB_ERR' }));
+});
+
+//Delete contact
+//Logged in user must delete the particular contact
+app.delete('/contact/:id', (request, response) => {
+	mongoClient
+		.connect(db_url, { useNewUrlParser: true })
+		.then((client) => {
+			const db = client.db('mydb');
+			const doc = db
+				.collection('contact')
+				.updateOne(
+					{ _id: request.params.id },
+					{ $pull: { 'contacts.phone': request.body.phone } }
+				);
+			doc.then((document) => response.status(200).json(document))
+				.catch(() => response.status(404).json({ error: 'Failed to remove contact' }))
+				.finally(() => client.close());
+		})
+		.catch(() => response.status(404).json({ error: 'DB_ERR' }));
+});
